bb8fa24074c0f80ddab774b9c945684764d8269a
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-07-05 13:49:13 +0530

    Agent rest

--- src/main/java/com/projects/digitalpostmasterrest/service/impl/UserServiceImpl.java
+++ src/main/java/com/projects/digitalpostmasterrest/service/impl/UserServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static com.projects.digitalpostmasterrest.constant.Contants.*;
+import static com.projects.digitalpostmasterrest.constant.Constants.*;
 
 @Slf4j
 @Service
